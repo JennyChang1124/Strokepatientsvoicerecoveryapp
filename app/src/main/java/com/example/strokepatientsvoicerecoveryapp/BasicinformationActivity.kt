f32d5ffb45504b2b08a33ae9a47efcf07b222e58
Kon245 <ganchia.li@gmail.com>
2023-03-26 23:47:21 +0800

    loginSignupDone

---
+++ app/src/main/java/com/example/strokepatientsvoicerecoveryapp/BasicinformationActivity.kt
@@ -0,0 +1,23 @@
+package com.example.strokepatientsvoicerecoveryapp
+
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.basic_information.*
+import kotlinx.android.synthetic.main.registerlogin.*
+
+
+class BasicinformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.basic_information)
+
+
+        //當按下送出鍵就會跳轉到主題選擇畫面
+        send.setOnClickListener {
+            val   intent = Intent(this,MainoptionActivity::class.java);
+            startActivity(intent)
+        }
+    }
+}
